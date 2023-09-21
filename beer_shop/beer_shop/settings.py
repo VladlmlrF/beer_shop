7d260d625291aa5a4377b7053f0f2a094de5d2e5
VladlmlrF <vladimirfalin84@gmail.com>
2023-09-21 15:31:40 +0700

    created app cart, created class Cart

--- beer_shop/beer_shop/settings.py
+++ beer_shop/beer_shop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -131,4 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
+CART_SESSION_ID = 'cart'
