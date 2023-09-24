77b8e8a4f41d305a3473cfd916bdfe410262cb28
VladlmlrF <vladimirfalin84@gmail.com>
2023-09-24 15:42:36 +0700

    created payment/webhooks

--- beer_shop/payment/urls.py
+++ beer_shop/payment/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from .webhooks import *
 
 app_name = 'payment'
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('process/', payment_process, name='process'),
     path('completed/', payment_completed, name='completed'),
     path('canceled/', payment_canceled, name='canceled'),
+    path('webhook/', stripe_webhook, name='stripe-webhook'),
 ]
